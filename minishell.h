dde75afcdecbdb548479aa10778c75987df34424
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2024-09-17 17:12:04 +0200

    Include libc headers

--- minishell.h
+++ minishell.h
@@ -6,10 +6,23 @@
 /*   By: lchauffo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 16:05:04 by lchauffo          #+#    #+#             */
-/*   Updated: 2024/09/16 16:06:07 by lchauffo         ###   ########.fr       */
+/*   Updated: 2024/09/17 17:11:47 by libousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <dirent.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <sys/ioctl.h>
+# include <curses.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
 #endif
