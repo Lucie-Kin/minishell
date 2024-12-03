552b480f5a78e21dfa72c67542126d06b250ad27
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2024-12-03 23:03:46 +0100

    Fix parenthesis syntax check

--- parser/extract_line/syntax_error.c
+++ parser/extract_line/syntax_error.c
@@ -6,16 +6,16 @@
 /*   By: lchauffo <lchauffo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 22:33:41 by libousse          #+#    #+#             */
-/*   Updated: 2024/11/25 17:48:33 by libousse         ###   ########.fr       */
+/*   Updated: 2024/12/03 23:00:15 by libousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../parser.h"
 
-static char	*get_syntax_err_msg(const char *tok);
 static int	check_meta_token(char **tok, size_t i, char **err_msg);
 static int	err_on_curr(char **tok, size_t i);
 static int	err_on_next(char **tok, size_t i);
+static char	*get_syntax_err_msg(const char *tok);
 
 int	check_for_syntax_errors(t_sh *sh, size_t index)
 {
@@ -46,6 +46,67 @@ int	check_for_syntax_errors(t_sh *sh, size_t index)
 	return (is_legal);
 }
 
+static int	check_meta_token(char **tok, size_t i, char **err_msg)
+{
+	if (err_on_curr(tok, i))
+	{
+		*err_msg = get_syntax_err_msg(tok[i]);
+		return (0);
+	}
+	else if (err_on_next(tok, i))
+	{
+		*err_msg = get_syntax_err_msg(tok[i + 1]);
+		return (0);
+	}
+	return (1);
+}
+
+/*
+	If no left operand (command can start with '(', '<', '>')
+	If '(' doesn't start the command and is preceded with non-meta
+	If bad token length
+	If ')' but no prior '('
+*/
+static int	err_on_curr(char **tok, size_t i)
+{
+	size_t	j;
+
+	if (!i && tok[i][0] != '(' && tok[i][0] != '<' && tok[i][0] != '>')
+		return (1);
+	else if (i && tok[i][0] == '(' && !is_metacharacter(tok[i - 1][0]))
+		return (1);
+	else if ((tok[i][0] == '&' && !tok[i][1])
+		|| (tok[i][0] == ';' && tok[i][1]))
+		return (1);
+	else if (tok[i][0] == ')')
+	{
+		j = i;
+		while (j > 0 && tok[j][0] != '(')
+			--j;
+		return (tok[j][0] != '(');
+	}
+	return (0);
+}
+
+/* If next token is unexpected */
+static int	err_on_next(char **tok, size_t i)
+{
+	if (tok[i][0] == ')' && tok[i + 1] && (tok[i + 1][0] == '('
+		|| !is_metacharacter(tok[i + 1][0])))
+		return (1);
+	else if (tok[i][0] == ';' && tok[i + 1] && (tok[i + 1][0] == ';'
+		|| tok[i + 1][0] == '|' || tok[i + 1][0] == '&'))
+		return (1);
+	else if ((tok[i][0] == '(' || tok[i][0] == '|' || tok[i][0] == '&')
+		&& tok[i + 1] && (tok[i + 1][0] == ')' || tok[i + 1][0] == ';'
+		|| tok[i + 1][0] == '|' || tok[i + 1][0] == '&'))
+		return (1);
+	else if ((tok[i][0] == '<' || tok[i][0] == '>')
+		&& (!tok[i + 1] || is_metacharacter(tok[i + 1][0])))
+		return (1);
+	return (0);
+}
+
 static char	*get_syntax_err_msg(const char *tok)
 {
 	char	*tmp1;
@@ -63,63 +124,4 @@ static char	*get_syntax_err_msg(const char *tok)
 		free(tmp2);
 	}
 	return (tmp1);
-}
-
-static int	check_meta_token(char **tok, size_t i, char **err_msg)
-{
-	if (err_on_curr(tok, i))
-	{
-		*err_msg = get_syntax_err_msg(tok[i]);
-		return (0);
-	}
-	else if (err_on_next(tok, i))
-	{
-		*err_msg = get_syntax_err_msg(tok[i + 1]);
-		return (0);
-	}
-	return (1);
-}
-
-// If no left operand (command can start with '(', '<', '>')
-// If '(' doesn't start the command and is preceded with non-meta
-// If bad token length
-// If ')' but no prior '('
-static int	err_on_curr(char **tok, size_t i)
-{
-	size_t	j;
-
-	if (!i && tok[i][0] != '(' && tok[i][0] != '<' && tok[i][0] != '>')
-		return (1);
-	else if (i && tok[i][0] == '(' && !is_metacharacter(tok[i - 1][0]))
-		return (1);
-	else if ((tok[i][0] == '&' && !tok[i][1])
-		|| (tok[i][0] == ';' && tok[i][1]))
-		return (1);
-	else if (tok[i][0] == ')')
-	{
-		j = 0;
-		while (j < i && tok[j][0] != '(')
-			++j;
-		return (j == i);
-	}
-	return (0);
-}
-
-// If next token is unexpected
-static int	err_on_next(char **tok, size_t i)
-{
-	if (tok[i][0] == ')' && tok[i + 1] && (tok[i + 1][0] == '('
-		|| tok[i + 1][0] == ')' || !is_metacharacter(tok[i + 1][0])))
-		return (1);
-	else if (tok[i][0] == ';' && tok[i + 1] && (tok[i + 1][0] == ';'
-		|| tok[i + 1][0] == '|' || tok[i + 1][0] == '&'))
-		return (1);
-	else if ((tok[i][0] == '(' || tok[i][0] == '|' || tok[i][0] == '&')
-		&& tok[i + 1] && (tok[i + 1][0] == ')' || tok[i + 1][0] == ';'
-		|| tok[i + 1][0] == '|' || tok[i + 1][0] == '&'))
-		return (1);
-	else if ((tok[i][0] == '<' || tok[i][0] == '>')
-		&& (!tok[i + 1] || is_metacharacter(tok[i + 1][0])))
-		return (1);
-	return (0);
 }
